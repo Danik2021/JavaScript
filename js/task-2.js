3a405242a07ab6b85e76cf1b6e8e8b08f4fa1164
danikyakimenko <danialyakimenko@gmail.com>
2024-10-15 17:37:11 +0200

    Task 1 and task 2 are done!

---
+++ js/task-2.js
@@ -0,0 +1,36 @@
+'use strict';
+
+class Storage {
+  #items;
+  constructor(settings) {
+    this.#items = settings;
+  }
+
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    this.#items = this.#items.filter(item => {
+      return item !== itemToRemove;
+    });
+  }
+}
+
+const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator', 'Scaner']);
+console.log(storage);
+
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem('Droid');
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem('Prolonger');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem('Scaner');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
