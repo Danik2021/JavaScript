2d7e4c1c9637ef1392b76ccb610b5b01fcc09356
danikyakimenko <danialyakimenko@gmail.com>
2024-10-22 14:20:24 +0200

    resole confilcts

--- js/task-1.js
+++ js/task-1.js
@@ -3,6 +3,7 @@
 const categoriesItemEl = document.querySelectorAll('.item');
 
 console.log(`Number of categories: ${categoriesItemEl.length}`);
+
 categoriesItemEl.forEach(item => {
   console.log(item.firstElementChild.textContent);
   console.log(`Elements: ${item.lastElementChild.children.length}`);
