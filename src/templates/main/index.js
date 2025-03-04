f2a78487dc29a3930162b84a39f5cf5e6bfd48c7
danikyakimenko <danialyakimenko@gmail.com>
2025-03-04 16:45:35 +0100

    help!

--- src/templates/main/index.js
+++ src/templates/main/index.js
@@ -2,8 +2,12 @@ import axios from "axios";
 
 import { refs } from "./scripts/refs";
 import { state } from "./scripts/state";
-import { onClickSearchButton, onSubmitForm, onClickShowMoreButton } from './scripts/handlers';
-import { Notification } from '../../models/notification.model';
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+import { onClickSearchButton, onSubmitForm, onClickShowMoreButton } from "./scripts/handlers";
+import { Notification } from "../../models/notification.model";
 
 refs.form.addEventListener("submit", onSubmitForm);
 refs.searchButton.addEventListener("click", onClickSearchButton);
@@ -13,65 +17,41 @@ const API_URL = "https://pixabay.com/api/";
 const key = "48781960-dc6df10b20f0dfee4adceb61a";
 
 const otherParams = {
-  image_type: "photo",
-  orientation: "horizontal",
-  safesearch: "true",
-  per_page: 20,
-  page: state.pageNum,
-}
+    image_type: "photo",
+    orientation: "horizontal",
+    safe_search: "true",
+    per_page: 20,
+    page: state.pageNum,
+};
 
 export async function fetchGalleryImages(query) {
-  try {
-    const dataFromApi = await axios.get(API_URL, {
-      params: {
-        key,
-        q: query,
-        ...otherParams,
-      },
-    });
+    try {
+        const dataFromApi = await axios.get(API_URL, {
+            params: {
+                key,
+                q: query,
+                ...otherParams,
+            },
+        });
 
-    const colection = dataFromApi.data.hits;
+        const colection = dataFromApi.data.hits;
 
-    if (!colection.length) {
-      if (!refs.buttonShowMore.classList.contains('hide')) refs.buttonShowMore.classList.add("hide");
-      throw new Error("Sorry, there are no images matching your search query. Please try again.");
-    }
+        if (!colection.length) {
+            if (!refs.buttonShowMore.classList.contains("hide")) refs.buttonShowMore.classList.add("hide");
+            throw new Error("Sorry, there are no images matching your search query. Please try again.");
+        }
+        refs.buttonShowMore.classList.remove("hide");
+        state.pageNum += 1;
 
-    refs.buttonShowMore.classList.remove("hide");
-    state.pageNum += 1;
-
-    // TODO: можна використати замість reduce:
-    // const massImages = colection
-    //     .map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
-    //         return `<div class="photo-card">
-    //                     <img src="${webformatURL}" alt="" loading="lazy" />
-    //             <div class="info">
-    //                     <p class="info-item">
-    //                         <b>Likes</b>
-    //                         <span>${likes}</span>
-    //                     </p>
-    //                     <p class="info-item">
-    //                         <b>Views</b>
-    //                         <span>${views}</span>
-
-    //                     </p>
-    //                     <p class="info-item">
-    //                         <b>Comments</b>
-    //                         <span>${comments}</span>
-    //                     </p>
-    //                     <p class="info-item">
-    //                         <b>Downloads</b>
-    //                         <span>${downloads}</span>
-    //                     </p>
-    //                  </div>
-    //             </div>`;
-    //     })
-    //     .join(" ");
-
-    const massImages = colection
-      .reduce((acc, { webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
-        return acc + `<div class="photo-card">
-                            <img src="${webformatURL}" alt="" loading="lazy" />
+        const massImages = colection.reduce(
+            (acc, { webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
+                return (
+                    acc +
+                    `<div class="photo-card">
+                    
+                    <a href="${largeImageURL}">
+                          <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+                    </a>
                     <div class="info">
                             <p class="info-item">
                                 <b>Likes</b>
@@ -91,11 +71,22 @@ export async function fetchGalleryImages(query) {
                                 <span>${downloads}</span>
                             </p>
                          </div>
-                    </div>`;
-      }, '');
+                    </div>`
+                );
+            },
+            ""
+        );
 
-    refs.container.insertAdjacentHTML("beforeend", massImages);
-  } catch (error) {
-    Notification.error(error.message);
-  }
+        refs.container.insertAdjacentHTML("beforeend", massImages);
+
+        if (refs.gallery !== null) refs.gallery.destroy();
+
+        refs.gallery = new SimpleLightbox(".gallery .photo-card a", {
+            caption: true,
+            captionsData: "alt",
+            captionDelay: 250,
+        });
+    } catch (error) {
+        Notification.error(error.message);
+    }
 }
--- src/templates/main/scripts/refs.js
+++ src/templates/main/scripts/refs.js
@@ -1,7 +1,8 @@
 export const refs = {
-  form: document.querySelector(".search-form"),
-  input: document.querySelector(".search-form__input"),
-  searchButton: document.querySelector(".search-form__button"),
-  container: document.querySelector(".gallery"),
-  buttonShowMore: document.querySelector(".gallery-button"),
+    gallery: null,
+    form: document.querySelector(".search-form"),
+    input: document.querySelector(".search-form__input"),
+    searchButton: document.querySelector(".search-form__button"),
+    container: document.querySelector(".gallery"),
+    buttonShowMore: document.querySelector(".gallery-button"),
 };
